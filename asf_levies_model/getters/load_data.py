fd93824e64d13c41f45ad96359578c1a46a9fce9
eglucas <elysialucas@gmail.com>
2024-09-05 13:10:14 +0100

    ofgem archetypes data load function

--- asf_levies_model/getters/load_data.py
+++ asf_levies_model/getters/load_data.py
@@ -110,7 +110,8 @@ def _get_charging_years(policy_df: pd.DataFrame, policy_acronym: str) -> list:
 def _check_updates_years(update_dates: list, charging_years: list):
     """Checks if number of dates of update and number of charging years are equal."""
     if len(update_dates) == len(charging_years):
-        print(f"Number of entries: {len(update_dates)}")
+        pass
+        # print(f"Number of entries: {len(update_dates)}")
     else:
         raise ValueError("Number of time periods do not match!")
 
@@ -300,7 +301,7 @@ def _get_lookup_periods(policy_df: pd.DataFrame) -> np.array:
 def _check_periods(charge_period_1: list, charge_period_2: list, lookup_period: list):
     """Checks if number of charge periods and lookup periods are equal."""
     if len(charge_period_1) == len(charge_period_2) == len(lookup_period):
-        print(f"Number of entries: {len(charge_period_1)}")
+        # print(f"Number of entries: {len(charge_period_1)}")
         return True
     else:
         return False
@@ -537,3 +538,223 @@ def tidy_tariff_table(input_df: pd.DataFrame, type_of_consumption: str) -> pd.Da
     )
 
     return tidy_df
+
+
+def ofgem_archetypes_data() -> pd.DataFrame:
+    """Pre-filled function to generate a dataframe with Ofgem archetype data."""
+    dataframe = pd.concat(
+        [
+            pd.Series(
+                [
+                    "Typical",
+                    "A1",
+                    "A2",
+                    "A3",
+                    "B4",
+                    "B5",
+                    "B6",
+                    "C7",
+                    "C8",
+                    "C9",
+                    "D10",
+                    "D11",
+                    "D12",
+                    "E13",
+                    "E14",
+                    "F15",
+                    "F16",
+                    "G17",
+                    "G18",
+                    "H19",
+                    "H20",
+                    "I21",
+                    "I22",
+                    "J23",
+                    "J24",
+                ],
+                name="AnnualConsumptionProfile",
+            ),
+            pd.Series(
+                [
+                    2700.0,
+                    2742.0,
+                    2849.0,
+                    3519.0,
+                    4811.0,
+                    6597.0,
+                    3028.0,
+                    3649.0,
+                    5587.0,
+                    3337.0,
+                    3881.0,
+                    2482.0,
+                    3952.0,
+                    5075.0,
+                    4070.0,
+                    6883.0,
+                    4317.0,
+                    5901.0,
+                    5294.0,
+                    4907.0,
+                    3143.0,
+                    4070.0,
+                    4684.0,
+                    4532.0,
+                    7523.0,
+                ],
+                name="ElectricitySingleRatekWh",
+            ),
+            pd.Series(
+                [
+                    11500.0,
+                    10933.0,
+                    9464.0,
+                    10622.0,
+                    0.0,
+                    0.0,
+                    10525.0,
+                    13119.0,
+                    0.0,
+                    13685.0,
+                    13981.0,
+                    8782.0,
+                    16065.0,
+                    16722.0,
+                    14606.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    11677.0,
+                    15461.0,
+                    18530.0,
+                    16330.0,
+                    0.0,
+                ],
+                name="GaskWh",
+            ),
+            pd.Series(
+                [
+                    None,
+                    15643,
+                    17327,
+                    18195,
+                    18776,
+                    22423,
+                    24869,
+                    29257,
+                    32240,
+                    32344,
+                    31819,
+                    40980,
+                    38927,
+                    38351,
+                    43026,
+                    46005,
+                    50721,
+                    44586,
+                    49265,
+                    52621,
+                    58924,
+                    59668,
+                    68332,
+                    74795,
+                    78813,
+                ],
+                name="GrossAnnualHouseholdIncome",
+            ),
+            pd.Series(
+                [
+                    "Typical",
+                    "Oldest and poorest",
+                    "Low-income older people in social housing",
+                    "Low-income adults with disability in social housing",
+                    "Old poor people with electric heating",
+                    "Pensioners with medical  equipment and/or electric heating",
+                    "Low-income couples/singles in small rented/social housing",
+                    "Council housing with children",
+                    "Electric council housing with 1 child",
+                    "Pensioners in semi-detached houses",
+                    "Disabled owner-occupiers",
+                    "Flats with good EPC",
+                    "Pensioners in mansions",
+                    "Families with disabilities",
+                    "Average families",
+                    "Mid income large families on other fuel",
+                    "Electric yuppies",
+                    "Welsh barns",
+                    "Other fuels",
+                    "Oil rural older couples",
+                    "High income without children",
+                    "High income families with 1 child",
+                    "Own detached houses with no children",
+                    "High income families with 2+ children",
+                    "Big rich rural homes with children",
+                ],
+                name="ArchetypeNickname",
+            ),
+            pd.Series(
+                [
+                    1_000_000,
+                    578_333,
+                    868_191,
+                    883_413,
+                    731_318,
+                    465_288,
+                    920_172,
+                    659_595,
+                    228_477,
+                    3_408_514,
+                    1_163_946,
+                    1_197_075,
+                    1_457_829,
+                    690_892,
+                    1_178_684,
+                    323_433,
+                    989_639,
+                    163_166,
+                    667_836,
+                    675_712,
+                    3_540_270,
+                    2_210_494,
+                    1_792_593,
+                    1_956_103,
+                    231_658,
+                ],
+                name="ArchetypeSize",
+            ),
+            pd.Series(
+                [
+                    "Gas",  # Typical
+                    "Gas",  # A1
+                    "Gas",  # A2
+                    "Gas",  # A3
+                    "Electricity",  # B4
+                    "Electricity/Other",  # B5
+                    "Gas",  # B6
+                    "Gas",  # C7
+                    "Electricity",  # C8
+                    "Gas",  # C9
+                    "Gas",  # D10
+                    "Gas",  # D11
+                    "Gas",  # D12
+                    "Gas",  # E13
+                    "Gas",  # E14
+                    "Electricity/Other",  # F15
+                    "Electricity",  # F16
+                    "Other",  # G17
+                    "Other",  # G18
+                    "Other",  # H19
+                    "Gas",  # H20
+                    "Gas",  # I21
+                    "Gas",  # I22
+                    "Gas",  # J23
+                    "Other",  # J24
+                ],
+                name="ArchetypeHeatingFuel",
+            ),
+        ],
+        axis=1,
+    )
+    return dataframe
